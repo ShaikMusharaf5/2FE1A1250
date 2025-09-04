c3374b923290ec71c6f8d2cb10fa5203659959a0
Musharaf <shaikmushed786@gmail.com>
2025-09-04 12:42:21 +0530

    Complete project

---
+++ models/Shorturl.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const clickSchema = new mongoose.Schema({
+  timestamp: { type: Date, default: Date.now },
+  referrer: { type: String },
+  sourceIP: { type: String }
+  // Add geo data here if you want
+});
+
+const shortUrlSchema = new mongoose.Schema({
+  url:         { type: String, required: true },
+  shortcode:   { type: String, required: true, unique: true },
+  createdBy:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt:   { type: Date, default: Date.now },
+  validity:    { type: Number, default: 30 }, // minutes
+  expiry:      { type: Date, required: true },
+  clicks:      [clickSchema]
+});
+
+module.exports = mongoose.model('ShortUrl', shortUrlSchema);
